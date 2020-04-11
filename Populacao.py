961e964d695231970222ffc59af067e54680bd66
maurifc <mauri_vrb@hotmail.com>
2020-04-11 14:31:08 -0300

    add funcoes reproducao e addDescendente; alterada funcao de fitnessMedio

--- Populacao.py
+++ Populacao.py
@@ -1,28 +1,68 @@
 from Individuo import Individuo
 import random
+import sys
 
 class Populacao(object):
 
     def __init__(self, tamanhoPopulacao, tamanhoIndividuo):
+        self.populacao = []
+        self.contadorGeracao = 1
+        self.geracoes = [1]
+        self.fitnessMedioDasGeracoes = []
+        self.individuoMaiorFitness = []
+        self.segIndividuoMaiorFitness = []
         self.tamanhoPopulacao = tamanhoPopulacao
         self.tamanhoIndividuo = tamanhoIndividuo
-        self.populacao = []
 
-    def gerarPopulacao(self):
+    def inicializar(self):
         for i in range(self.tamanhoPopulacao):
             individuo = Individuo(self.tamanhoIndividuo)
             self.populacao.append(individuo)
             print(individuo.materialGenetico)
+        self.fitnessMedioDasGeracoes.append(self.calcularFitnessMedio())
     
-    def calcularFitness(self):
+    def calcularFitnessMedio(self):
+        fitnessTotal = 0
         for i in range(self.tamanhoPopulacao):
-            self.populacao[i].calcularFitness()
-            print("Individuo {}: {}".format(i, self.populacao[i].fitness))
+            fitnessTotal += self.populacao[i].calcularFitness()
+        
+        return fitnessTotal / self.tamanhoPopulacao
     
-    def fazerMutacao(self):
+    def mutacao(self):
         #Sorteia um indivíduo da população
         posicaoIndividuo = random.randint(0, len(self.populacao)-1)
         print("Posicao Indiviuo:" + str(posicaoIndividuo))
         self.populacao[posicaoIndividuo].fazerMutacao()
+
+    def selecao(self):
+        self.individuoMaiorFitness = ['1']
+        self.segIndividuoMaiorFitness = ['0']
+    
+    def reproducao(self):
+        indvHalf = len(self.individuoMaiorFitness)/2
+        filho = []
+
+        for i in range(len(self.individuoMaiorFitness)):
+            if i < indvHalf:
+                filho.append(self.individuoMaiorFitness[i])
+            else:
+                filho.append(self.segIndividuoMaiorFitness[i])
+
+        return filho
+
+    def adicionarDescendente(self, descendente):
+        menorFitness = sys.float_info.max
+
+        for indice,individuo in enumerate(self.populacao):
+            if (individuo.calcularFitness() < menorFitness):
+                indiceMenorFitness = indice
+    
+        self.populacao[indiceMenorFitness] = descendente
+
+        #Incrementa o contador de geracoes
+        self.contadorGeracao += 1
+        #Coloca mais uma geracao no array de geracoes e seu respectivo fitness médio no array 'fitness'
+        self.geracoes.append(self.contadorGeracao)
+        self.fitnessMedioDasGeracoes.append(self.calcularFitnessMedio())
 
         
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import random
 import math
 from Populacao import Populacao
+import graficoFitnessMedio as grafico
 
 TAMANHO_INDIVIDUO = 4
 TAMANHO_POPULACAO = 2
 
 populacao = Populacao(TAMANHO_POPULACAO, TAMANHO_INDIVIDUO)
-populacao.gerarPopulacao()
-populacao.calcularFitness()
-populacao.fazerMutacao()
+populacao.inicializar()
 
+
+populacao.selecao()
+populacao.reproducao()
+populacao.mutacao()
+
+grafico.plotar(populacao)
